244431a88e247c9b96f6e308c6ef556976efb72b
Anrich <anrich@debian>
2014-05-25 08:58:58 +0800

    Which was forked should be Etherner/ChatServer, though.

--- ArduinoBasic.ino
+++ ArduinoBasic.ino
@@ -1,18 +1,17 @@
 /*
-  Telnet client
+ Chat  Server
 
- This sketch connects to a a telnet server (http://www.google.com)
- using an Arduino Wiznet Ethernet shield.  You'll need a telnet server
- to test this with.
- Processing's ChatServer example (part of the network library) works well,
- running on port 10002. It can be found as part of the examples
- in the Processing application, available at
- http://processing.org/
+ A simple server that distributes any incoming messages to all
+ connected clients.  To use telnet to  your device's IP address and type.
+ You can see the client's input in the serial monitor as well.
+ Using an Arduino Wiznet Ethernet shield.
 
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
 
- created 14 Sep 2010
+ created 18 Dec 2009
+ by David A. Mellis
  modified 9 Apr 2012
  by Tom Igoe
 
@@ -22,23 +21,24 @@
 #include <Ethernet.h>
 
 // Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
+// The IP address will be dependent on your local network.
+// gateway and subnet are optional:
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,1,177);
+IPAddress ip(192,168,1, 177);
+IPAddress gateway(192,168,1, 1);
+IPAddress subnet(255, 255, 0, 0);
 
-// Enter the IP address of the server you're connecting to:
-IPAddress server(1,1,1,1);
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 23 is default for telnet;
-// if you're using Processing's ChatServer, use  port 10002):
-EthernetClient client;
+// telnet defaults to port 23
+EthernetServer server(23);
+boolean alreadyConnected = false; // whether or not the client was connected previously
 
 void setup() {
-  // start the Ethernet connection:
-  Ethernet.begin(mac, ip);
+  // initialize the ethernet device
+  Ethernet.begin(mac, ip, gateway, subnet);
+  // start listening for clients
+  server.begin();
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
@@ -46,45 +46,32 @@ void setup() {
   }
 
 
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 10002)) {
-    Serial.println("connected");
-  }
-  else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
+  Serial.print("Chat server address:");
+  Serial.println(Ethernet.localIP());
 }
 
-void loop()
-{
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
+void loop() {
+  // wait for a new client:
+  EthernetClient client = server.available();
 
-  // as long as there are bytes in the serial queue,
-  // read them and send them out the socket if it's open:
-  while (Serial.available() > 0) {
-    char inChar = Serial.read();
-    if (client.connected()) {
-      client.print(inChar); 
+  // when the client sends the first byte, say hello:
+  if (client) {
+    if (!alreadyConnected) {
+      // clead out the input buffer:
+      client.flush();
+      Serial.println("We have a new client");
+      client.println("Hello, client!");
+      alreadyConnected = true;
     }
-  }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    // do nothing:
-    while(true);
+    if (client.available() > 0) {
+      // read the bytes incoming from the client:
+      char thisChar = client.read();
+      // echo the bytes back to the client:
+      server.write(thisChar);
+      // echo the bytes to the server as well:
+      Serial.write(thisChar);
+    }
   }
 }
 
